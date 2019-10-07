a6f9e93515455f0658152f5c2e2af1bfbb349ed7
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-10-07 09:17:52 +0200

    Adds webpack

---
+++ webpack/index.babel.js
@@ -0,0 +1,50 @@
+import * as path from 'path';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+
+const cleanWebpackPlugin = new CleanWebpackPlugin({
+  cleanOnceBeforeBuildPatterns: [
+    'client-side/*.js',
+    'server-side/*.js',
+  ],
+  verbose: true,
+});
+
+require('dotenv').config();
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  mode: isProduction ? 'production' : 'development',
+  output: {
+    path: path.resolve(__dirname, '../src/'),
+    filename: '[name].js',
+    libraryTarget: 'commonjs',
+  },
+  entry: {
+    'client-side/index':
+      path.resolve(__dirname, '../src/client-side/index.ts'),
+    'server-side/index':
+      path.resolve(__dirname, '../src/server-side/index.ts'),
+  },
+  resolve: {
+    mainFiles: [ 'index' ],
+    extensions: [ '.js', '.ts' ],
+    modules: [ 'node_modules' ],
+  },
+  node: {
+    fs: 'empty',
+  },
+  plugins: [
+    cleanWebpackPlugin,
+  ],
+  module: {
+    rules: [
+      {
+        test: /.*ts$/,
+        use: [
+          'babel-loader',
+          'ts-loader',
+        ],
+      },
+    ],
+  },
+};
