0f4b097ad5a09aa68d93cf4e996d1094b33c4add
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-10-10 11:05:57 +0200

    Server | add optional parameter to render function

--- src/server-side/index.ts
+++ src/server-side/index.ts
@@ -1,20 +1,17 @@
-import {
-  HTMLElementProps,
-  buildRender,
-} from '../common/';
+import { HTMLElementProps } from '../common/types';
 import { SSRDocument } from './SSRDocument';
 import { SSRElement } from './SSRElement';
 import { SSRFragment } from './SSRFragment';
+import { buildRender } from '../common/';
 
 type SSRCollection = (SSRElement | SSRFragment)[];
 
 const _render = buildRender(new SSRDocument());
 
-const render = (
-  (props: HTMLElementProps): string => (
+const render =
+  (props: HTMLElementProps = {}): string => (
     (_render(props) as unknown as SSRCollection)
       .map(child => child.toString()).join('')
-  )
-);
+  );
 
 export default render;
