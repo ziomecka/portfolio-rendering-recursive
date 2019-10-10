8c7fbe295bac9b84acdad51bf88c64fe0e7cfd33
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-10-10 16:04:44 +0200

    Common | moves types to seperate dir

---
+++ src/common/types/index.ts
@@ -0,0 +1,12 @@
+export { HTMLTag } from './HTMLTag.types';
+export {
+  AppendChild,
+  BuildRender,
+  CreateElement,
+} from './common.types';
+export { EventHandlers } from './event.types';
+export {
+  CustomDocument,
+  CustomHTMLElement,
+  HTMLElementProps,
+} from './html.types';
