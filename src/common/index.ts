32d5b06db5e56e37ebc3d13662eb3bf7647ca9b5
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-10-10 10:21:39 +0200

    Common | Adds in buildRender function a check if props truthy, if is not returns null

--- src/common/index.ts
+++ src/common/index.ts
@@ -10,51 +10,55 @@ import {
 const buildRender: buildRender = (
   (document: CustomDocument) => (
     (props: HTMLElementProps): CustomHTMLCollection => {
-      const createHtmlElement = ({
-        HTMLTag,
-        value,
-        className,
-      }: HTMLElementProps): CustomHTMLElement => {
-        const $element = document.createElement(HTMLTag);
-        $element.innerText = value;
-        if (className) {
-          $element.classList.add(className);
-        }
-        return $element;
-      };
-
-      const appendChild = (
-        { children, HTMLTag, ...otherProps }: HTMLElementProps,
-        fragment: CustomDocumentFragment = document.createDocumentFragment(),
-      ): CustomDocumentFragment => {
-        if (!children || !children.length) {
-          fragment.append(createHtmlElement({ HTMLTag, ...otherProps }));
-          return fragment;
-        }
-
-        const $element =
-          HTMLTag && createHtmlElement({ HTMLTag, ...otherProps });
-        const $fragment = document.createDocumentFragment();
-
-        children.forEach(child => {
-          if (typeof child === 'string') {
-            appendChild({ value: child }, $fragment);
-          } else {
-            appendChild(child, $fragment);
+      if (props) {
+        const createHtmlElement = ({
+          HTMLTag,
+          value,
+          className,
+        }: HTMLElementProps): CustomHTMLElement => {
+          const $element = document.createElement(HTMLTag);
+          $element.innerText = value;
+          if (className) {
+            $element.classList.add(className);
           }
-        });
+          return $element;
+        };
 
-        if ($element) {
-          $element.append($fragment);
-          fragment.append($element);
-        } else {
-          fragment.append($fragment);
-        }
+        const appendChild = (
+          { children, HTMLTag, ...otherProps }: HTMLElementProps,
+          fragment: CustomDocumentFragment = document.createDocumentFragment(),
+        ): CustomDocumentFragment => {
+          if (!children || !children.length) {
+            fragment.append(createHtmlElement({ HTMLTag, ...otherProps }));
+            return fragment;
+          }
 
-        return fragment;
-      };
+          const $element =
+            HTMLTag && createHtmlElement({ HTMLTag, ...otherProps });
+          const $fragment = document.createDocumentFragment();
 
-      return appendChild(props).children as unknown as CustomHTMLCollection;
+          children.forEach(child => {
+            if (typeof child === 'string') {
+              appendChild({ value: child }, $fragment);
+            } else {
+              appendChild(child, $fragment);
+            }
+          });
+
+          if ($element) {
+            $element.append($fragment);
+            fragment.append($element);
+          } else {
+            fragment.append($fragment);
+          }
+
+          return fragment;
+        };
+
+        return appendChild(props).children as unknown as CustomHTMLCollection;
+      } else {
+        return null;
+      }
     }
   )
 );
