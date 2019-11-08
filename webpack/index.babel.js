3e6405b96d499375592bb51c411245f71b3e7ae2
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-11-08 10:56:20 +0100

    Bundles to devBundle or prodBundle, publishes to npm devBundle

--- webpack/index.babel.js
+++ webpack/index.babel.js
@@ -1,22 +1,23 @@
 import * as path from 'path';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
+require('dotenv').config();
+const isProduction = process.env.NODE_ENV === 'production';
+const outputDir = isProduction ? 'prodBundle' : 'devBundle';
+
 const cleanWebpackPlugin = new CleanWebpackPlugin({
-  cleanOnceBeforeBuildPatterns: [
-    'client-side/*.js',
-    'server-side/*.js',
+  cleanBeforeBuildPatterns: [
+    `${ outputDir }/**/*.js`,
   ],
   verbose: true,
 });
 
-require('dotenv').config();
-const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   mode: isProduction ? 'production' : 'development',
   devtool: !isProduction ? 'source-map' : false,
   output: {
-    path: path.resolve(__dirname, '../src/'),
+    path: path.resolve(__dirname, '../', outputDir),
     filename: '[name].js',
     libraryTarget: 'commonjs',
   },
